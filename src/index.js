c65d5e0861074ce33689d4dda791b065e24753d8
hisui <ruru.apple.1212@gmail.com>
2023-01-01 17:23:17 +0900

    first_commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // 開発モードのみ
+  // コンポーネントを初回に2回マウントし、レンダリング回数が増える
+  // 　→バグを検出しやすくするため
+  // <React.StrictMode>
+    <App />
+  // </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
